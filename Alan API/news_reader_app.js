82f3046686dd78239b1db2d3d61d7bceb15a9b86
Prapti Jaduvanshi <67221447+praptijaduvanshi@users.noreply.github.com>
2022-07-14 09:13:21 +0700

    Update news_reader_app.js

--- Alan API/news_reader_app.js
+++ Alan API/news_reader_app.js
@@ -3,7 +3,7 @@ intent('What does this app do?', 'What can I do here?',
        reply('This is a news application. And, I will be your voice assistant.'));
 
 //intent('Start a command', (p) => {
- //   p.play({ command: 'testCommand'});
+//   p.play({ command: 'testCommand'});
 //})
 
 const API_KEY= '97ed84ac9af848dca864a0aeb2a3503e';
@@ -13,24 +13,24 @@ let savedArticles = [];
 // News by Source
 intent('Give me the news from $(source* (.*))', (p) => {
     let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}`;
-    
+
     if(p.source.value) {
-        NEWS_API_URL = `${NEWS_API_URL}&sources=${p.source.value.toLowerCase().split(" ").join('-')}`
+        NEWS_API_URL = `${NEWS_API_URL}&sources=${p.source.value.toLowerCase().split(" ").join('-')}`;
     }
-    
+
     api.request(NEWS_API_URL, (error, response, body) => {
-        const { articles } = JSON.parse(body);
+        const { totalResults, articles } = JSON.parse(body);
         
-        if(!articles.length) {
+        if( totalResults==0 ) {
             p.play('Sorry, please try searching for news from a different source');
             return;
         }
-        
+
         savedArticles = articles;
-        
+
         p.play({ command: 'newHeadlines', articles });
         p.play(`Here are the (latest|recent) news from ${p.source.value}.`);
-  
+
         p.play('Would you like me to read the headlines?');
         p.then(confirmation);
     });
@@ -39,24 +39,24 @@ intent('Give me the news from $(source* (.*))', (p) => {
 // News by Term
 intent('what\'s up with $(term* (.*))', (p) => {
     let NEWS_API_URL = `https://newsapi.org/v2/everything?apiKey=${API_KEY}`;
-    
+
     if(p.term.value) {
         NEWS_API_URL = `${NEWS_API_URL}&q=${p.term.value}`
     }
-    
+
     api.request(NEWS_API_URL, (error, response, body) => {
-        const { articles } = JSON.parse(body);
-        
-        if(!articles.length) {
+        const { totalResults, articles } = JSON.parse(body);
+
+        if( totalResults==0 ) {
             p.play('Sorry, please try searching for something else.');
             return;
         }
-        
+
         savedArticles = articles;
-        
+
         p.play({ command: 'newHeadlines', articles });
         p.play(`Here are the (latest|recent) articles on ${p.term.value}.`);
-        
+
         p.play('Would you like me to read the headlines?');
         p.then(confirmation);
     });
@@ -67,31 +67,31 @@ const CATEGORIES = ['business', 'entertainment', 'general', 'health', 'science',
 const CATEGORIES_INTENT = `${CATEGORIES.map((category) => `${category}~${category}`).join('|')}`;
 
 intent(`(show|what is|tell me|what's|what are|what're|read) (the|) (recent|latest|) $(N news|headlines) (in|about|on|) $(C~ ${CATEGORIES_INTENT})`,
-  `(read|show|get|bring me|give me) (the|) (recent|latest) $(C~ ${CATEGORIES_INTENT}) $(N news|headlines)`, (p) => {
+       `(read|show|get|bring me|give me) (the|) (recent|latest) $(C~ ${CATEGORIES_INTENT}) $(N news|headlines)`, (p) => {
     let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}&country=us`;
-    
+
     if(p.C.value) {
         NEWS_API_URL = `${NEWS_API_URL}&category=${p.C.value}`
     }
-    
+
     api.request(NEWS_API_URL, (error, response, body) => {
         const { articles } = JSON.parse(body);
-        
+
         if(!articles.length) {
             p.play('Sorry, please try searching for a different category.');
             return;
         }
-        
+
         savedArticles = articles;
-        
+
         p.play({ command: 'newHeadlines', articles });
-        
+
         if(p.C.value) {
             p.play(`Here are the (latest|recent) articles on ${p.C.value}.`);        
         } else {
             p.play(`Here are the (latest|recent) news`);   
         }
-        
+
         p.play('Would you like me to read the headlines?');
         p.then(confirmation);
     });
@@ -104,7 +104,7 @@ const confirmation = context(() => {
             p.play(`${savedArticles[i].title}`);
         }
     })
-    
+
     intent('no', (p) => {
         p.play('Sure, sounds good to me.')
     })
